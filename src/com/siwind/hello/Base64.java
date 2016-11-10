7a8987c2e255f818bff8ba3d524c0ea4af72e27a
wangyq <yinqingwang@gmail.com>
2016-11-10 10:42:48 +0800

    add base64 example

--- src/com/siwind/hello/Base64.java
+++ src/com/siwind/hello/Base64.java
@@ -1,5 +1,7 @@
 package com.siwind.hello;
 
+//import sun.misc.BASE64Decoder;
+
 /**
  * Created by admin on 2016/11/10.
  */
@@ -36,10 +38,30 @@ public class Base64 {
         return authorizationHeaderName + ": " + authorizationHeaderValue;
     }
 
+    public static String enc(){
+        String username = "myusername";
+        String password = "myPassword";
+
+        String usernameAndPassword = username + ":" + password;
+        String authorizationHeaderName = "Authorization";
+        String authorizationHeaderValue = "Basic " + Base64Util.encode( usernameAndPassword.getBytes() );
+        return authorizationHeaderName + ": " + authorizationHeaderValue;
+    }
+    public static String dec(String str){
+        return new String(Base64Util.decode(str));
+    }
+
     public static void test(){
+        System.out.println(enc());
+        System.out.println(dec("bXl1c2VybmFtZTpteVBhc3N3b3Jk"));
         System.out.println(enc7());
         System.out.println(enc8());
         System.out.println(dec8("bXl1c2VybmFtZTpteVBhc3N3b3Jk"));
+        System.out.println("testing0 = " + Base64Util.encode("testing0".getBytes()));
+        System.out.println(dec8("dGVzdGluZzA="));
+        System.out.println(dec("dGVzdGluZzA="));
+        System.out.println(dec8("AAAAAAAAAAABAg=="));
+        System.out.println(dec("AAAAAAAAAAABAg=="));
     }
 
     /**
