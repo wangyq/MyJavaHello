a3bab7e03fe9df171d2b77c2def80af46c448292
wangyq <yinqingwang@gmail.com>
2017-09-14 23:32:23 +0800

    ok

--- src/com/siwind/hello/Inequality.java
+++ src/com/siwind/hello/Inequality.java
@@ -1,14 +1,43 @@
 package com.siwind.hello;
 
+import java.math.BigDecimal;
+
 public class Inequality {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Test1();
+		//Test1();
+		Test2();
 	}
 
+	public static void  Test2() {
+		TestFuncName(10);
+		TestFuncName(100);
+		TestFuncName(200);
+	}
+	public static void TestFuncName(int n) {
+		int  i=0;
+		StringBuilder str = new StringBuilder("");
+		
+		for(i=1;i<n;i+=2){
+			str.append("Sqrt[x^2 + 1 + 2*x*Cos[" + i +"Pi/"+n+"]] " );  //Sqrt[x^2 + 1 + 2*x*Cos[Pi/10]]
+			if( i<n-1){
+				str.append("+");
+			}
+		}
+		str.append("-(");
+		for(i=2;i<n-1;i+=2){
+			str.append("Sqrt[x^2 + 1 + 2*x*Cos[" + i +"Pi/"+n+"]] " );  //Sqrt[x^2 + 1 + 2*x*Cos[Pi/10]]
+			if( i <n-2){
+				str.append("+");
+			}
+		}
+		str.append(")");
+		
+		System.out.println(str);
+	}
 	public static void Test1() {
-		TestInequality(100, 0, 2, 0.1);
+		TestInequality(10, 0, 1.0, 0.001);
 	}
 	/**
 	 * sqrt(x^2 + 2x*cos[k*PI/2n] + 1
@@ -39,8 +68,9 @@ public class Inequality {
 		for(k=2;k<2*n-1; k+=2){
 			v -= TrigoFunc(x, n, k);
 		}
-		return v -1;
+		return v ;
 	}
+	
 	/**
 	 * 
 	 * @param n
